19336a3bd5ecc77ce458e000c38a3b8acd62e12b
Débora <dbucci@protonmail.com>
2021-09-08 22:50:34 -0300

    Make the stickyNav function to work only when the window is 700px or less.

--- js/script.js
+++ js/script.js
@@ -5,25 +5,29 @@ const header = document.querySelector("header");
 const navbar = document.getElementById("nav-bar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
-// FUNCTION :
-const stickyNav = function (entries) {
-  console.log(entries);
+const checkSize = function () {
+  navbar.classList.remove("sticky");
 
-  const [entry] = entries;
-  console.log(entry);
+  if (window.innerWidth < 700) {
+    console.log("small");
+    const stickyNav = function (entries) {
+      const [entry] = entries;
 
-  if (!entry.isIntersecting) navbar.classList.add("sticky");
-  else navbar.classList.remove("sticky");
+      if (!entry.isIntersecting) navbar.classList.add("sticky");
+      else navbar.classList.remove("sticky");
+    };
 
-  console.log(navbar.classList);
+    const navbarObserver = new IntersectionObserver(stickyNav, {
+      root: null,
+      threshold: 0,
+      rootMargin: `-${navbarHeight}px`,
+    });
+
+    navbarObserver.observe(header);
+  } else if (window.innerWidth >= 700) {
+    navbar.classList.remove("sticky");
+  }
 };
 
-// NAVBAR OBSERVER DEFINED
-const navbarObserver = new IntersectionObserver(stickyNav, {
-  root: null,
-  threshold: 0,
-  rootMargin: `-${navbarHeight}px`,
-});
-
-// CALL NAVBAR OBSERVER :
-navbarObserver.observe(header);
+checkSize();
+window.addEventListener("resize", checkSize);
